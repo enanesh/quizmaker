88dc97eff60c79d89c92416d036d33a7715d75da
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-03-14 20:30:40 -0700

    index.js file is updated for models

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const Answer = require('./Answer');
 const Category = require('./Category');
 const Question = require('./Question');
 const QuizUser = require('./QuizUser');
-const Test = require('./Test');
+const Quiz = require('./Quiz');
 const User = require('./User');
 
-module.exports = { Answer, Category, Question, QuizUser, Test, User };
+module.exports = { Answer, Category, Question, QuizUser, Quiz, User };
