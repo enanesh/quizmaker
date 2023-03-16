d6aaead521e25e0612d757d2e675d18782a4e26f
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-03-15 18:08:41 -0700

    Adds Quiz page

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Section from './components/Section';
+import Section from './components/pages/Section';
 import { setContext } from '@apollo/client/link/context';
 import RenderQuiz from "./pages/RenderQuiz";
 import Quizes from './components/pages/Quizes';
@@ -40,7 +40,7 @@ function App() {
      <Routes>
        <Route exact path='/' element={<Section/>} />
        <Route exact path='/quizes' element={<Quizes />} />
-       <Route path="/quiz" element={<RenderQuiz />} />
+       {/* <Route path="/quiz" element={<RenderQuiz />} /> */}
      </Routes>
     </Router>
     </ApolloProvider>   
--- client/src/components/pages/Quizes.js
+++ client/src/components/pages/Quizes.js
@@ -3,7 +3,67 @@ import { Link } from 'react-router-dom';
 
 const AppQuizes = () => {
     return (
-        <h1>Quizes</h1>
+        
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg m-4">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Title
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Category
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Questions
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Time (min)
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Action</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Test 1
+                </th>
+                <td class="px-6 py-4">
+                    Math
+                </td>
+                <td class="px-6 py-4">
+                    50
+                </td>
+                <td class="px-6 py-4">
+                    60
+                </td>
+                <td class="px-6 py-4 text-right">
+                    <a href="/quiz" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Take it</a>
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                   Test 2
+                </th>
+                <td class="px-6 py-4">
+                    JavaScript
+                </td>
+                <td class="px-6 py-4">
+                    100
+                </td>
+                <td class="px-6 py-4">
+                    60
+                </td>
+                <td class="px-6 py-4 text-right">
+                    <a href="/quiz" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Take it</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 
     );
 }
