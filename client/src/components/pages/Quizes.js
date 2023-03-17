4a499659152da385083e4d37ec023dcbc0af1c6c
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-03-16 21:04:36 -0700

    Changes table

--- client/src/components/pages/Quizes.js
+++ client/src/components/pages/Quizes.js
@@ -3,66 +3,166 @@ import { Link } from 'react-router-dom';
 
 const AppQuizes = () => {
     return (
-        
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg m-4">
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" className="px-6 py-3">
-                    Title
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Questions
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Time (min)
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    <span className="sr-only">Action</span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Test 1
-                </th>
-                <td className="px-6 py-4">
-                    Math
-                </td>
-                <td className="px-6 py-4">
-                    50
-                </td>
-                <td className="px-6 py-4">
-                    60
-                </td>
-                <td className="px-6 py-4 text-right">
-                    <a href="/quiz" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Take it</a>
-                </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                   Test 2
-                </th>
-                <td className="px-6 py-4">
-                    JavaScript
-                </td>
-                <td className="px-6 py-4">
-                    100
-                </td>
-                <td className="px-6 py-4">
-                    60
-                </td>
-                <td className="px-6 py-4 text-right">
-                    <a href="/quiz" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Take it</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+        <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
+                        <div className="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-white shadow-lg px-12">
+                            <div className="flex justify-between">
+                                <div className="inline-flex border rounded w-7/12 px-2 lg:px-6 h-12 bg-transparent">
+                                    <div className="flex flex-wrap items-stretch w-full h-full mb-6 relative">
+                                        <div className="flex">
+                                            <span className="flex items-center leading-normal bg-transparent rounded rounded-r-none border border-r-0 border-none lg:px-3 py-2 whitespace-no-wrap text-grey-dark text-sm">
+                                                <svg width="18" height="18" className="w-4 lg:w-auto" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M8.11086 15.2217C12.0381 15.2217 15.2217 12.0381 15.2217 8.11086C15.2217 4.18364 12.0381 1 8.11086 1C4.18364 1 1 4.18364 1 8.11086C1 12.0381 4.18364 15.2217 8.11086 15.2217Z" stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <path d="M16.9993 16.9993L13.1328 13.1328" stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                        <input type="text" className="flex-shrink flex-grow flex-auto leading-normal tracking-wide w-px flex-1 border border-none border-l-0 rounded rounded-l-none px-3 relative focus:outline-none text-xxs lg:text-xs lg:text-base text-gray-500 font-thin" placeholder="Search"></input>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
+                            <table className="min-w-full">
+                                <thead>
+                                    <tr>
+                                        <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">ID</th>
+                                        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Title</th>
+                                        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Category</th>
+                                        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Description</th>
+                                        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Number of questions</th>
+                                        <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Creator</th>
+                                        <th className="px-6 py-3 border-b-2 border-gray-300"></th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white">
+                                        <tr>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <div className="text-sm leading-5 text-gray-800">#1</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="text-sm leading-5 text-blue-900">Test</div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Category name</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Test description</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+                                                <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                <span aria-hidden className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                <span className="relative text-xs">10</span>
+                                            </span>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">Test owner</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
+                                                <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Take</button>
+                                            </td>
+                                  </tr>
+                                       <tr>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <div className="text-sm leading-5 text-gray-800">#1</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="text-sm leading-5 text-blue-900">Test</div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Category name</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Test description</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+                                                <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                <span aria-hidden className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                <span className="relative text-xs">10</span>
+                                            </span>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">Test owner</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
+                                                <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Take</button>
+                                            </td>
+                                  </tr>
+                                  <tr>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <div className="text-sm leading-5 text-gray-800">#1</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="text-sm leading-5 text-blue-900">Test</div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Category name</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Test description</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+                                                <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                                                <span aria-hidden className="absolute inset-0 bg-red-200 opacity-50 rounded-full"></span>
+                                                <span className="relative text-xs">10</span>
+                                            </span>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">Test owner</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
+                                                <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Take</button>
+                                            </td>
+                                  </tr>
+                                  <tr>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <div className="text-sm leading-5 text-gray-800">#1</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="text-sm leading-5 text-blue-900">Test</div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Category name</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Test description</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+                                                <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                <span aria-hidden className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                <span className="relative text-xs">10</span>
+                                            </span>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">Test owner</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
+                                                <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Take</button>
+                                            </td>
+                                  </tr>
+                                  <tr>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <div className="text-sm leading-5 text-gray-800">#1</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                                <div className="text-sm leading-5 text-blue-900">Test</div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Category name</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">Test description</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+                                                <span className="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
+                                                <span aria-hidden className="absolute inset-0 bg-orange-200 opacity-50 rounded-full"></span>
+                                                <span className="relative text-xs">10</span>
+                                            </span>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">Test owner</td>
+                                            <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
+                                                <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Take</button>
+                                            </td>
+                                  </tr>
+                                </tbody>
+                            </table>
+                          <div className="sm:flex-1 sm:flex sm:items-center sm:justify-between mt-4 work-sans">
+                <div>
+                </div>
+            </div>
+            </div>     
+        </div>
 
 
     );
