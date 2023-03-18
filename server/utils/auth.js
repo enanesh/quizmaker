abc164561dc83b8c4eb9e9823da4ee772a0a0196
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-18 08:31:58 -0700

    Moves Session SECRET to .env varabile

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretsshhhhh';
+const SECRET = process.env.SECRET;
 const expiration = '2h';
 
 module.exports = {
@@ -18,7 +18,7 @@ module.exports = {
     }
 
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, SECRET, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
@@ -29,6 +29,6 @@ module.exports = {
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ data: payload }, SECRET, { expiresIn: expiration });
   },
 };
