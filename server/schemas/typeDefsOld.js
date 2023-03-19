8592d6f001c8a6ca33b5f201f747349452dc37c3
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-03-19 09:01:52 -0700

    login.js file is changed to handle request from client to server. typeDeff.js troubleshooting is going on. added export to client mutation. added auth.js file to client part to use jwt and use local storage.

--- server/schemas/typeDefsOld.js
+++ server/schemas/typeDefsOld.js
@@ -1,25 +1,41 @@
 const typeDefs = `#graphql
+  "Users that can take or make quizes"
   type User {
+    "Unique Identifier"
     _id: ID
+    "The name a user shows the world"
     username: String!
+    "First Name"
     firstname: String
+    "Last Name"
     lastname: String
+    "Email Address"
     email: String!
+    "Hashed Password"
     password: String!
+    "Quizzes Completed"
+    completedquizes: [CompletedQuizes]
   }
 
+  "Oh look! A Quiz!!"
   type Quiz {
+    "Quiz Identifier"
     quizID: ID!
+    "When was this made?"
     createdAt: String!
+    "Quiz Creator"
     owner: String!
+    "Questions"
     questions: Question!
+    "How did they do??"
+    results: [Results]
   }
 
   type QuizUser {
     user: ID!
     quiz: ID!
-    score: Int
-    completedAt: String
+    score: Number
+    completedAt: Date
   }
 
   type Question {
@@ -49,30 +65,44 @@ const typeDefs = `#graphql
 
   type Query {
     users: [User]
+  "Quiz by ID"
     getQuizById(quizId: ID!): Quiz!
+  "Get quiz array for homepage grid"
     getQuestionsByQuizId(quizId: ID!): Quiz!
+  "Get all quizes"
     getAllQuizzes: [Quiz]
+  "Get quizes by Creator"
     getAllQuizzesByOwner(owernerId: ID!): [Quiz]!
+  "Get Quizes you've Taken"
     getAllQuizzesByStudent(studentId: ID!): [Quiz]!
+  "Get Answers by Quiz ID"
     getAnswersByQuizId(quizId: ID!): [Answer!]!
-    getMyProfile: User!
+  "Get Profile Information"
+    getMyProfile(context): User!
+  "Search for User by username or email"
     getUserByUserNameOrEmail(username: String!, email: String!): User!
   }
 
   type Mutation {
+    "Add a User"
     addUser(username: String!, firstname: String!, lastname: String!, email: String!, password: String!): Auth
+    "Login"
     login(email: String!, password: String!): Auth
-    // look to this: quizData: Object!
-    createQuiz(quizData: Object!): Quiz
-    updateQuiz(quizId: ID!, title: String!): Quiz
-    createQuestion(quizId: ID!, questionData: Object!): Question
-    updateQuestion(questionId: ID!, updatedQuestionData: Object!): Question
-    saveAnswer(answerData: Object!): Answer
-    deleteQuiz(quizId: ID!): Quiz
-    deleteQuestion(questionId: ID!): Question
+    "Create a Quiz!"
+    createQuiz(quizData: Object!, context)
+   "Update a Quiz"
+    updateQuiz(quizId: ID! title: String!, context)
+   "Create a Question"
+    createQuestion(quizId: ID!, questionData: Object!, context)
+   "Update a Question"
+    updateQuestion(questionId: ID!, updatedQuestionData: Object!, context)
+   "Save you Answer"
+    saveAnswer(answerData: Object!, context)
+    "Delete a Quiz"
+    deleteQuiz(quizId: ID!, context)
+    "Delete a Question"
+    deleteQuestion(questionId: ID!, context)
   }
 `;
-createQuiz(quizData: {title: String, options: String[]}): Quiz
-
 
 module.exports = typeDefs;
