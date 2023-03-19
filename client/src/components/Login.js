949aa9d3ca6a6e7d00a2e210962930de19de7cc4
enanesh <edna.ganesha.gr@gmail.com>
2023-03-18 19:40:04 -0700

    Indentation changes

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -30,12 +30,12 @@ const Login = () => {
             .then((res) => {
                 console.log(res);
             })
-            
+
         setEmail('');
         setPassword('');
     };
     return (
-        <div  class="bg-confetti relative lg:py-20">
+        <div class="bg-confetti relative lg:py-20">
             <div class="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl
       xl:px-5 lg:flex-row">
                 <div class="flex flex-col items-center w-full pt-5 pr-10 pb-20 pl-10 lg:pt-20 lg:flex-row">
@@ -54,26 +54,26 @@ const Login = () => {
                                 </div>
                                 <div class="relative">
                                     <p class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">Email</p>
-                                    <input 
-                                    placeholder="123@ex.com" 
-                                    type="text" 
-                                    name="email"
-                                    value={email}
-                                    onChange={handleInputChange}
-                                    class="border placeholder-gray-400 focus:outline-none
+                                    <input
+                                        placeholder="123@ex.com"
+                                        type="text"
+                                        name="email"
+                                        value={email}
+                                        onChange={handleInputChange}
+                                        class="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"/>
                                 </div>
                                 <div class="relative">
                                     <p class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                   absolute">Password</p>
-                                    <input 
-                                    placeholder="*****" 
-                                    type="password"
-                                    name="password"
-                                    value={password}
-                                    onChange={handleInputChange} 
-                                    class="border placeholder-gray-400 focus:outline-none
+                                    <input
+                                        placeholder="*****"
+                                        type="password"
+                                        name="password"
+                                        value={password}
+                                        onChange={handleInputChange}
+                                        class="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"/>
                                 </div>
