17f387153da87520a05c06392ae16885862d1eb2
Jamey Wicklund <thebluwiz@icloud.com>
2023-03-21 13:09:59 -0700

    Adds path package to server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 // Import the ApolloServer class
 const { ApolloServer } = require("@apollo/server");
