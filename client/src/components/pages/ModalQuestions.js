2c8d8326a57c8c5e52dcbfe6c49ca21745ccb530
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-03-21 22:12:12 -0700

    Adds student field to quiz

--- client/src/components/pages/ModalQuestions.js
+++ client/src/components/pages/ModalQuestions.js
@@ -4,6 +4,7 @@ import TemporaryQuizCard from "../TemporaryQuizCard";
 import { ADD_QUESTION, ADD_ANSWER, ADD_QUIZ } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
 const uuid = require("../../utils/uuid");
+const quizId = uuid();
 
 export default function Modal(props) {
   const [questions, setQuestions] = useState([{}]);
@@ -61,10 +62,20 @@ export default function Modal(props) {
       typeMatch = "radiogroup";
     }
 
+    const quizMutation = await addQuiz({
+      variables: {
+        quizId: quizId,
+        title: props.initInfo[2],
+        description: quizCategory,
+        student: quizAssignedTo
+      },
+    });
+
     const questionId = uuid();
+
     const questionMutation = await addQuestion({
       variables: {
-        quizId: props.quizId,
+        quizId: quizId,
         questionId: questionId,
         questiontext: currentQuestionState.question,
         correctanswer: answerMatch,
@@ -76,24 +87,6 @@ export default function Modal(props) {
       variables: {
         questionId: questionId,
         selectedanswer: currentQuestionState.option1,
-      },
-    });
-    const answerTwoMutation = await addAnswer({
-      variables: {
-        questionId: questionId,
-        selectedanswer: currentQuestionState.option2,
-      },
-    });
-    const answerThreeMutation = await addAnswer({
-      variables: {
-        questionId: questionId,
-        selectedanswer: currentQuestionState.option3,
-      },
-    });
-    const answerFourMutation = await addAnswer({
-      variables: {
-        questionId: questionId,
-        selectedanswer: currentQuestionState.option4,
       },
     });
   };
